7aa66ce3f21e62deebc2ef585e1615b1833c2519
German Ulrich <gerulrich@gmail.com>
2023-06-03 01:30:36 -0300

    Controller para channels

--- src/routes/channels.js
+++ src/routes/channels.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const  {requestValidator, validateJWT, rolesAllowed}  = require('../middlewares');
 const { getChannelById, getChannels, createChannel, updateChannel, deleteChannel } = require('../controllers/channels.controller');
 
@@ -7,7 +8,8 @@ const router = Router();
 
 router.get('/channels/:id', [
     validateJWT,
-    rolesAllowed(['USER_ROLE', 'ADMIN_ROLE']),
+    rolesAllowed('USER_ROLE', 'ADMIN_ROLE'),
+    check('id', 'An invalid channel id was supplied').isMongoId(),
     requestValidator
 ], getChannelById);
 
@@ -26,9 +28,10 @@ router.post('/channels', [
 ], createChannel);
 
 
-router.patch('/channels/:id',  [
+router.put('/channels/:id',  [
     validateJWT,
     rolesAllowed('ADMIN_ROLE'),
+    check('id', 'An invalid channel id was supplied').isMongoId(),
     requestValidator
 ], updateChannel);
 
